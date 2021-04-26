9b47fb2dbd25f60132997ff2d8b4c44be51033cc
itstam95 <nhattam253@gmail.com>
2021-04-26 20:40:49 +0700

    migration product table

--- database/migrations/2021_04_23_053453_create_products_table.php
+++ database/migrations/2021_04_23_053453_create_products_table.php
@@ -13,7 +13,7 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('products', function(Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('images')->nullable()->comment('Image for product');
